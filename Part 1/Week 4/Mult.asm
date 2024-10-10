9a7da7d22b0196f03492dd7f2509bb37ea029e8d
Guilherme Lampert <126016021+guilherme-lampert@users.noreply.github.com>
2024-10-10 12:10:18 -0300

    Created Week 4 folder and Mult.asm

---
+++ Part 1/Week 4/Mult.asm
@@ -0,0 +1,43 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.\
+
+   // i = 0
+   @i
+   M=0
+
+   // R2 = 0
+   @R2
+   M=0
+
+(LOOP)
+   // if (i == R0) goto END
+   @i
+   D=M
+   @R0
+   D=D-M
+   @END
+   D;JEQ
+   
+   // R2 = R2 + R1
+   @R1
+   D=M
+   @R2
+   M=D+M
+   
+   // i = i + 1
+   @i
+   M=M+1
+   
+   // jump to the start of the loop
+   @LOOP
+   0;JMP
+
+(END)
+   @END
+   0;JMP
